9b3358629b779c6823d8a20e9e63bfb8091dfe9e
Vitaliy Emporopulo <vemporop@redhat.com>
2024-04-10 13:04:40 +0300

    Fix base image

--- container-image.sh
+++ container-image.sh
@@ -4,9 +4,12 @@ set -e
 
 RHSM_ORG_FILE=.rhsm_org
 RHSM_ACTIVATIONKEY_FILE=.rhsm_activationkey
+
+BASE_IMAGE=quay.io/fabiendupont/rhel9-nvidia:9.4-550.54.15
 IMAGE_NAME=quay.io/vemporop/bootc:9.4-pytorch
 
 podman build \
     --secret id=RHSM_ORG,src=${RHSM_ORG_FILE} \
 	--secret id=RHSM_ACTIVATIONKEY,src=${RHSM_ACTIVATIONKEY_FILE} \
-    -t $IMAGE_NAME .
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    -t ${IMAGE_NAME} .
